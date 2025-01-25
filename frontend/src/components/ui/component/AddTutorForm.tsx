ce90e3401321cfa5ad9e449c1fd3853299af76f2
masood <masoodacheampong@gmail.com>
2025-01-25 22:19:40 +0000

    changed structure of modal for adding a tutor.

--- frontend/src/components/ui/component/AddTutorForm.tsx
+++ frontend/src/components/ui/component/AddTutorForm.tsx
@@ -1,29 +1,31 @@
-import { tutorSignUpFormSchema as formSchema } from "@/validation/Tutorschema"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { useAppDispatch, useAppSelector } from "@/store/hooks"
-import { Button } from "@/components/ui/button"
+import { tutorSignUpFormSchema as formSchema } from "@/validation/Tutorschema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { loadTutors, tutorSignUp, } from "@/api/slices/tutorthunk"
+} from "@/components/ui/form";
+import { Dialog, DialogTitle, DialogContent } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { loadTutors, tutorSignUp } from "@/api/slices/tutorthunk";
+import { useToast } from "@/hooks/use-toast";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
-
-import { useToast } from "@/hooks/use-toast"
-
-
-import { Label } from "@/components/ui/label"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-export function AddTutor() {
-
+export function AddTutorModal({
+  isOpen,
+  setClose,
+}: {
+  isOpen: boolean;
+  setClose: (open: boolean) => void;
+}) {
   const dispatch = useAppDispatch();
   const { toast } = useToast();
   const { loading } = useAppSelector((state) => state.tutor);
@@ -44,8 +46,6 @@ export function AddTutor() {
 
   // Submit handler
   async function onSubmit(values: z.infer<typeof formSchema>) {
-
-
     const result = await dispatch(tutorSignUp(values));
     console.log(result);
     if (result.meta.requestStatus === "fulfilled") {
@@ -57,10 +57,10 @@ export function AddTutor() {
           </pre>
         ),
       });
-      dispatch(loadTutors())
-
+      dispatch(loadTutors());
       // Notify parent component to update the tutor list
       // onAddTutor(values); // Pass the new tutor data
+      onClose();
       reset();
     } else {
       toast({
@@ -74,129 +74,156 @@ export function AddTutor() {
     }
   }
 
+  const onClose = () => {
+    setClose(false);
+  };
+
   return (
-    <div className="lg:min-h-[503px] lg:w-[544px] lg:pr-12 text-left rounded-lg">
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogTitle>Register a tutor</DialogTitle>
+        <div className="lg:min-h-[503px] lg:w-[544px] lg:pr-12 text-left rounded-lg">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className=" flex justify-center items-center flex-col   "
+            >
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem className="flex  flex-col">
+                    <FormLabel className="text-sm">Firstname</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Firstname"
+                        {...field}
+                        className="h-7"
+                      />
+                    </FormControl>
 
-      <Form {...form} >
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem className="flex  flex-col">
+                    <FormLabel className="text-sm">Lastname</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Lastname"
+                        {...field}
+                        className="h-7"
+                      />
+                    </FormControl>
 
-        <form onSubmit={form.handleSubmit(onSubmit)} className=" flex justify-center items-center flex-col   " >
-          <FormField
-            control={form.control}
-            name="firstName"
-            render={({ field }) => (
-              <FormItem className="flex  flex-col">
-                <FormLabel className="text-sm">Firstname</FormLabel>
-                <FormControl>
-                  <Input placeholder="Firstname" {...field} className="h-7" />
-                </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="flex   flex-col">
+                    <FormLabel className="text-sm">Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Email" {...field} className="h-7" />
+                    </FormControl>
 
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="lastName"
-            render={({ field }) => (
-              <FormItem className="flex  flex-col">
-                <FormLabel className="text-sm">Lastname</FormLabel>
-                <FormControl>
-                  <Input placeholder="Lastname" {...field} className="h-7" />
-                </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="contact"
+                render={({ field }) => (
+                  <FormItem className="flex   flex-col">
+                    <FormLabel className="text-sm">Contact</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Contact" {...field} className="h-7" />
+                    </FormControl>
 
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem className="flex   flex-col">
-                <FormLabel className="text-sm">Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="Email" {...field} className="h-7" />
-                </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                <FormMessage />
-              </FormItem>
-            )}
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem className="flex    flex-col">
+                    <FormLabel className="text-sm text-left">
+                      Password
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Password"
+                        {...field}
+                        className="h-7"
+                      />
+                    </FormControl>
 
-          />
-          <FormField
-            control={form.control}
-            name="contact"
-            render={({ field }) => (
-              <FormItem className="flex   flex-col">
-                <FormLabel className="text-sm">Contact</FormLabel>
-                <FormControl>
-                  <Input placeholder="Contact" {...field} className="h-7" />
-                </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="gender"
+                render={({ field }) => (
+                  <FormItem className="flex   flex-col">
+                    <FormLabel className="text-sm text-left">Gender</FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                        className="flex pr-28 pb-2"
+                      >
+                        <div className="flex items-center space-x-2">
+                          <RadioGroupItem value="male" id="Male" />
+                          <Label htmlFor="Male">Male</Label>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                          <RadioGroupItem value="female" id="Female" />
+                          <Label htmlFor="Female">Female</Label>
+                        </div>
+                      </RadioGroup>
+                    </FormControl>
 
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="registeredCode"
+                render={({ field }) => (
+                  <FormItem className="flex  flex-col pb-3">
+                    <FormLabel className="text-sm">
+                      Registeration Code
+                    </FormLabel>
+                    <FormControl>
+                      <Input placeholder="" {...field} className="h-7" />
+                    </FormControl>
 
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem className="flex    flex-col">
-                <FormLabel className="text-sm text-left">Password</FormLabel>
-                <FormControl>
-                  <Input placeholder="Password" {...field} className="h-7" />
-                </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="gender"
-            render={({ field }) => (
-              <FormItem className="flex   flex-col">
-                <FormLabel className="text-sm text-left">Gender</FormLabel>
-                <FormControl>
-                  <RadioGroup onValueChange={field.onChange}
-                    defaultValue={field.value} className="flex pr-28 pb-2">
-                    <div className="flex items-center space-x-2">
-                      <RadioGroupItem value="male" id="Male" />
-                      <Label htmlFor="Male">Male</Label>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                      <RadioGroupItem value="female" id="Female" />
-                      <Label htmlFor="Female">Female</Label>
-                    </div>
-                  </RadioGroup>
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="registeredCode"
-            render={({ field }) => (
-              <FormItem className="flex  flex-col pb-3">
-                <FormLabel className="text-sm">Registeration Code</FormLabel>
-                <FormControl>
-                  <Input placeholder="" {...field} className="h-7" />
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <Button type="submit" className="px-20 mt-5">
-
-            {loading ? "Registering..." : "Register Tutor"}
-          </Button>
-        </form>
-      </Form>
-    </div>
+              <Button type="submit" className="px-20 mt-5">
+                {loading ? "Registering..." : "Register Tutor"}
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
